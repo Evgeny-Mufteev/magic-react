7524f2a64d4eb46551afd7d776fbcd73816cc769
Mufteev Evgeny <evgenymufteev@gmail.com>
2024-04-20 14:44:12 +0300

    AppRouter Config for router

--- src/pages/MainPage/ui/MainPage.async.tsx
+++ src/pages/MainPage/ui/MainPage.async.tsx
@@ -4,4 +4,4 @@ export const MainPageAsync = lazy(() => new Promise(resolve => {
   // @ts-ignore
   // ТАК В РЕАЛЬНЫХ ПРОЕКТАХ НЕ ДЕЛАТЬ!!!!!
   setTimeout(() => resolve(import('./MainPage')), 1500)
-}))
+}));
