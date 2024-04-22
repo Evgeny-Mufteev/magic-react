53b0a35fb3c610c84e62420f60db3954967486a5
Mufteev Evgeny <evgenymufteev@gmail.com>
2024-04-22 22:41:51 +0300

    Webpack hot module replacement and Babel Extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -7,6 +7,26 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
     const svgLoader = {
         test: /\.svg$/,
         use: ['@svgr/webpack']
+  }
+  
+      const babelLoader = {
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader",
+            options: {
+                presets: ['@babel/preset-env'],
+                "plugins": [
+                    [
+                        "i18next-extract",
+                        {
+                            locales: ['ru', 'en'],
+                            keyAsDefaultValue: true
+                        }
+                    ],
+                ]
+            }
+        }
     }
 
     const cssLoader = {
@@ -45,9 +65,10 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
     }
 
     return [
-        fileLoader,
-        svgLoader,
-        typescriptLoader,
-        cssLoader,
+      fileLoader,
+      svgLoader,
+      babelLoader,
+      typescriptLoader,
+      cssLoader,
     ]
 }
