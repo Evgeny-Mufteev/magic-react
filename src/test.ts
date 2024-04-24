8beb5219a281234e4f9dcec90ddb96bd329035a6
Mufteev Evgeny <evgenymufteev@gmail.com>
2024-04-24 23:11:42 +0300

    add eslint fix files

--- src/test.ts
+++ src/test.ts
@@ -1,4 +1,4 @@
 export function SomeFn(arg: number): string {
-  console.log("RANDOM FUNCTION sdfsf ssdvfsrf2342  ");
-  return ""
+    console.log('RANDOM FUNCTION sdfsf ssdvfsrf2342  ');
+    return '';
 }
